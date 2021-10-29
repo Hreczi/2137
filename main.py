b5c3b0d4f19f390246dbcd1f92f95980eb94c4e7
hreczi <piterowy134@wp.pl>
2021-10-29 17:41:53 +0200

    2137

---
+++ main.py
@@ -0,0 +1,46 @@
+from dhooks import Webhook, Embed
+import datetime
+import time
+
+hook = Webhook("https://discord.com/api/webhooks/903667555451932683/NjXmQn24h8lar_52Bhqnu3sYVmb8BHhCOpW_apgeUMNNSJ2uz1bawbH348OV-tGtPidh")
+
+print('włączono')
+
+papiezowa = Embed(
+    description = 'Papieżowa!',
+    color=2137,
+    )
+
+img = "https://img.joemonster.org/upload/qok/1320524f8aa4e271421065324509.gif"
+
+img1 = "https://piekarniagrzybki.pl/wp-content/uploads/2017/12/kremowka.jpg"
+
+img2 = "https://c.tenor.com/SuS8w88rMhYAAAAd/mozna-jak.gif"
+
+papiezowa.set_image(img)
+
+papiezowa.set_author(name='21:37!!', icon_url=img1)
+
+koniec = Embed(
+    description = 'Koniec papieżowej',
+    color = 2138,
+)
+
+koniec.set_image(img2)
+
+koniec.set_author(name = '21:38', icon_url=img1)
+
+
+while True:
+    now = datetime.datetime.now().time()
+
+    if now.hour == 21 and now.minute == 37:
+
+        hook.send(embed=papiezowa)
+        print('wyslano')
+
+        time.sleep(60)
+
+        hook.send(embed=koniec)
+
+        time.sleep(60)
